8ae4be9584a56cdf972d259c20a2d8f75d3a90eb
AmongPanda <MishsLap24680>
2023-04-19 23:11:09 +0300

    f

--- Time/Lab/organizer/models.py
+++ Time/Lab/organizer/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 
-
+##########################################################################################################
+class Organization(models.Model):
+    name_organizer = models.CharField(null=True, verbose_name='Название организации', max_length=1000)
+    email = models.EmailField(null=True)
+    number_phone = models.CharField(null=True, max_length=20)
+##########################################################################################################
 class Event(models.Model):
     title = models.CharField(null=True, verbose_name='Название', max_length=100)
-    Age = (
-    ('0+', '0+'),
-    ('6+', '6+'),
-    ('12+', '12+'),
-    ('18+', '18+'),
-    )
-    age = models.CharField(null=True, choices=Age, max_length=100, verbose_name='Возраст')
-
+    age = models.IntegerField(null=True, max_length=100, verbose_name='Возраст')
     date_time = models.DateTimeField(null=True)
-    price = models.CharField(null=True, max_length=100, verbose_name='Цена')
-    description_event = models.TextField(null=True, verbose_name='', max_length=1000)
+    price = models.DecimalField(null=True, max_length=100, verbose_name='Цена')
+    description_event = models.TextField(null=True, verbose_name='Описание', max_length=1000)
     image = models.ImageField(null=True, verbose_name='Фото', upload_to='media')
     organizer = models.CharField(null=True, verbose_name='Организатор', max_length=500)
     
@@ -23,13 +21,13 @@ class Event(models.Model):
     class Meta:
         verbose_name = 'Мероприятия'
         verbose_name_plural = 'мероприятия'
-
+##########################################################################################################
 
 class Ticket(models.Model):
     name_ticket = models.CharField(max_length=1000, verbose_name='Название')
-    description_ticket = models.TextField(null=True, verbose_name='', max_length=100)
+    description_ticket = models.TextField(null=True, verbose_name='Описание', max_length=100)
     image_ticket = models.ImageField(null=True, verbose_name='Фото')
-    price_ticket = models.CharField(null=True, max_length=100, verbose_name='Цена')
+    price_ticket = models.DecimalField(null=True, max_length=100, verbose_name='Цена')
     
     def __str__(self):
         return self.name_ticket
